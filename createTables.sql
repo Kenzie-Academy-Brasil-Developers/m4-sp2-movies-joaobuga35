c9ad103813f7fbccfe518f85abb9acd9ffa1bcb5
joaobuga35 <joaobuga35@gmail.com>
2023-02-04 22:14:08 -0300

    tests with routes

--- createTables.sql
+++ createTables.sql
@@ -1,8 +1,15 @@
+CREATE TABLE movies_favorites (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  description TEXT,
+  duration INTEGER NOT NULL,
+  price INTEGER NOT NULL
+);
+
+
 INSERT INTO 
 	movies_favorites("name",description,duration,price);
 VALUES ('Quebrando tudo', ' Filme loco', 160, 25);
-
-
 
 SELECT 
   *
